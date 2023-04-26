da00e3b5971ee5f781ce80e3ee88b476a9313da0
Faysal-Mia <60579168+Faysal-Mia@users.noreply.github.com>
2023-04-26 23:24:06 +0600

    Add files via upload

---
+++ module 4/assignment 1/5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d",&tk);
+    if(tk<=1000){
+        printf("Bad luck!");
+    }
+    else if(tk>1000){
+        printf("I will buy Punjabi\n");
+        tk = tk-1000;
+        if(tk>=500){
+            printf("I will buy new shoes\nAlisa will buy new shoes");
+        }
+    }
+    return 0;
+}
