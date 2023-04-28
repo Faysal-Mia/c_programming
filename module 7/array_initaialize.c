6cf93aca81ac5db5a5e7af8d38a99ff1deb13968
Faysal-Mia <60579168+Faysal-Mia@users.noreply.github.com>
2023-04-28 17:56:23 +0600

    Add files via upload

---
+++ module 7/array_initaialize.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int ar[6]={2,6,3,4,9,1};
+    for(int i=0;i<6;i++){
+    printf("%d\n",ar[i]);
+    
+    }
+
+    return 0;
+}
---
+++ module 7/array_io_1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int ar[3];
+    ar[0]= 10;
+    ar[1] = 20;
+    ar[2] = 50;
+    printf("%d %d %d", ar[0],ar[1],ar[2]);
+    
+    return 0;
+}
